4c17f54425b6302f29cdf23f4d5a6c8e1f301465
ailin-carabotta <ailin@thenode.gg>
2026-09-04 19:45:49 +0200

    Connecter Realtime Database

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 window.ROUE_FIREBASE_CONFIG = Object.freeze({
   apiKey: "AIzaSyBa2ipmbTnku1oXYnr2h4NaKvPtv8-m9K0",
   authDomain: "roue-champions-obs.firebaseapp.com",
-  databaseURL: "URL_DATABASE_A_REMPLACER",
+  databaseURL: "https://roue-champions-obs-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "roue-champions-obs",
   storageBucket: "roue-champions-obs.firebasestorage.app",
   messagingSenderId: "806521449649",
