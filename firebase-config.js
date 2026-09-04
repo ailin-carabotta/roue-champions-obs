166bf3d9ee8c9a4140cd9bbb2646020d64e352d5
ailin-carabotta <ailin@thenode.gg>
2026-09-04 19:37:35 +0200

    Ajouter firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+window.ROUE_FIREBASE_CONFIG = Object.freeze({
+  apiKey: "A_REMPLACER",
+  authDomain: "A_REMPLACER.firebaseapp.com",
+  databaseURL: "https://A_REMPLACER-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "A_REMPLACER",
+  appId: "A_REMPLACER"
+});
